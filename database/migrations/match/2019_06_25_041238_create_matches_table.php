116bdda165be431d8d58c00876e9d53f2823af03
Joe Dineen <MY_NAME@example.com>
2019-06-28 17:29:02 +0100

    migration update

--- database/migrations/match/2019_06_25_041238_create_matches_table.php
+++ database/migrations/match/2019_06_25_041238_create_matches_table.php
@@ -16,7 +16,7 @@ class CreateMatchesTable extends Migration
         Schema::connection('match')->create('matches', function (Blueprint $table) {
             $table->integer('gameCreation')->nullable();
             $table->integer('gameDuration')->nullable();
-            $table->bigIncrements('gameId');
+            $table->integer('gameId');
             $table->string('gameMode')->nullable();
             $table->string('gameType')->nullable();
             $table->string('gameVersion')->nullable();

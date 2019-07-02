05ca10294d6cba148f82c46ffb691079bea1ce55
Joe Dineen <MY_NAME@example.com>
2019-07-02 01:57:22 +0100

    champion page updates

--- resources/views/champions/show.blade.php
+++ resources/views/champions/show.blade.php
@@ -7,8 +7,8 @@
             <img src="/datadragon/9.13.1/img/champion/{{ $indiv_champion->id }}.png">
             <h1>{{ $indiv_champion->name }}</h1>
             <h4>{{ $indiv_champion->title }}</h4>
+            <div class="queue_type">Ranked Solo</div>
             <h5>{{ $indiv_champion->tags[0] }}</h5>
-            <div>Ranked Solo</div>
         </div>
         <div class="top_right">
             <div><img src=""></div>

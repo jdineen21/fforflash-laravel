d432d81b3018c509ebc546d0df556816ef46de38
Joe Dineen <MY_NAME@example.com>
2019-07-02 22:36:25 +0100

    react installed

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,28 +1,11 @@
-<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" type="text/css" />
-<div class="navbar">
-    <ul>
-        <li>
-            <a href="/" >Home</a>
-        </li>
-        <li>
-            <a href="/tier" >Tier List</a>
-        </li>
-        <li>
-            <a href="/champion-select" >Champ Select</a>
-        </li>
-        <li>
-            <a href="/summoner-info" >Summoner Info</a>
-        </li>
-        <li>
-            <a href="/contact" >Contact</a>
-        </li>
-        <li>
-            <form action="http://www.fforflash.com/search" method="get">
-                <input type="text" name="query" placeholder="Search This Site" class="textbox">
-                <div class="submit_div">
-                    <input type="submit">
-                </div>
-            </form>
-        </li>
-    </ul>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>React App</title>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="js/index.js"></script>
+    </body>
+</html>
